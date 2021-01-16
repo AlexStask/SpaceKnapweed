2644c6857225feb4935c90943936b49f9bc79b51
Alex Stasko <55051020+AlexStask@users.noreply.github.com>
2021-01-16 14:44:29 +0300

    add sounds.js file

---
+++ scripts/sounds.js
@@ -0,0 +1,9 @@
+function playBackgroundSound() {
+    let audio;
+    audio = new Audio('./media/sounds/background-sound.mov');
+    audio.play();
+}
+
+window.addEventListener('click', function() {
+    //playBackgroundSound();
+});
